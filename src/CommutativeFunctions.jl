bb7ab2123dcdddd7f46cbaf33e80ee7ee1e82ff5
jishnub <jishnub@users.noreply.github.com>
2021-01-28 19:11:19 +0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/CommutativeFunctions.jl
@@ -0,0 +1,5 @@
+module CommutativeFunctions
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CommutativeFunctions
+using Test
+
+@testset "CommutativeFunctions.jl" begin
+    # Write your tests here.
+end
